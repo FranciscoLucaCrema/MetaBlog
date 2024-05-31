33d9c0dd94a602cb4015295a50fd52da76251670
Francisco Crema <francisco.crema@miracledevs.com>
2024-05-31 13:07:19 -0300

    create div wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,13 @@ import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <>
+    <div className="app">
       <NavBar />
 
       <BrowserRouter>
         <Routes></Routes>
       </BrowserRouter>
-    </>
+    </div>
   );
 }
 
